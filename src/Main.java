9158c00fbef67a09334e20c112403d28b12f4a3b
Илья Авдеев <ilia.avdeev1013@gmail.com>
2022-06-13 16:50:34 +0300

    обновил гитигнор

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrace = 3000;
+        int CostOfOneMile = 20;
+        int NumberOfBonusMiles = TicketPrace / CostOfOneMile;
+        System.out.println(NumberOfBonusMiles);
+
+    }
+}
